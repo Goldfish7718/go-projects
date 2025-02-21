54fa496446579717c005113fb17c0b1bb4db4336
Tejas Nanoti <tejasnanoti2@gmail.com>
2025-02-21 21:46:21 +0530

    Complete calculator project;

--- calc-api/main.go
+++ calc-api/main.go
@@ -8,8 +8,12 @@ import (
 func main () {
 	var input1 int;
 	var input2 int;
+	var ch int;
 
+	
 	fmt.Println("Welcome to calculator API")
+	fmt.Println("1. Add\n2. Subtract\n3. Multiply\n4. Divide")
+	fmt.Scanln(&ch)
 
 	fmt.Println("Enter 1st number")
 	fmt.Scanln(&input1)
@@ -17,6 +21,17 @@ func main () {
 	fmt.Println("Enter 1st number")
 	fmt.Scanln(&input2)
 
-	sum := utils.Add(input1, input2)
-	fmt.Println(sum)
+	switch ch {
+	case 1:
+		fmt.Println(utils.Add(input1, input2))
+			
+	case 2:
+		fmt.Println(utils.Subtract(input1, input2))
+
+	case 3:
+		fmt.Println(utils.Multiply(input1, input2))
+
+	case 4:
+		fmt.Println(utils.Divide(input1, input2))
+	}
 }
--- calc-api/utils/functions.go
+++ calc-api/utils/functions.go
@@ -3,3 +3,15 @@ package utils
 func Add(x int, y int) int {
 	return x + y
 }
+
+func Subtract(x int, y int) int {
+	return x - y
+}
+
+func Divide(x int, y int) int {
+	return x / y
+}
+
+func Multiply(x int, y int) int {
+	return x * y
+}
