90e48f2470128345b9e9c247f9aa8de0a247bff2
Tejas Nanoti <tejasnanoti2@gmail.com>
2025-03-08 20:09:45 +0530

    feat: Add greeting;

--- api-tester/main.go
+++ api-tester/main.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"api-tester/subforms"
+	"fmt"
 	"log"
 
 	"github.com/charmbracelet/huh"
 )
 
-func main() {
+func getBaseUrl() string {
 	var baseUrl string = "http://localhost:3000"
-	var choice string
 
 	if err := huh.NewForm(
 		huh.NewGroup(
 			huh.NewInput().
-				Title("Welcome to API tester. Enter your base URL:").
+				Title("Enter your base URL:").
 				Value(&baseUrl),
 		),
 	).Run(); err != nil {
 		log.Fatal(err)
 	}
+
+	return baseUrl
+}
+
+func main() {
+	var choice string
+
+	fmt.Println("Welcome to API tester!")
+
+	baseUrl := getBaseUrl()
 
 	for {
 		if err := huh.NewForm(
@@ -28,6 +38,7 @@ func main() {
 					Title("Select an action").
 					Options(
 						huh.NewOption("Make a new request", "new"),
+						huh.NewOption("Set base url", "baseUrl"),
 						huh.NewOption("Exit", "exit"),
 					).
 					Value(&choice),
@@ -39,6 +50,9 @@ func main() {
 		switch choice {
 		case "new":
 			subforms.RequestSubform(baseUrl)
+
+		case "baseUrl":
+			baseUrl = getBaseUrl()
 
 		case "exit":
 			return
